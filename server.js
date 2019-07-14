4f5bd20bdcde4e4817f1f14d8834ee5532f3e7fd
ghehd231 <ghehd231@naver.com>
2019-07-14 18:09:05 +0900

    server npm install , add routes

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+/** routing 경로 */
+const userRouter = require('./routes/api/userRouter');
+const profileRouter = require('./routes/api/profileRouter');
+const postRouter = require('./routes/api/postRouter');
+
+const app = express();
+
+/** use route */
+app.use('/api/user', userRouter);
+app.use('/api/profile', profileRouter);
+app.use('/api/post', postRouter);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server running on port ${port}`)); 
